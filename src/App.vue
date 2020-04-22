36f971fd165c93c8a2487b0982daac64b9b989b2
ZAP <zhangaiping11011@163.com>
2020-04-22 21:19:27 +0800

    项目目录划分

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,16 @@
+
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-  import Axios from 'axios'
-  import Jsonp from 'jsonp'
   export default {
     name: "App",
-    mounted() {
-      // CORS跨域  这个easy-mock时常会出问题，要等他本身好了，才能用。
-      // let url = 'https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list';
-      // Axios.get(url).then(()=>{
-      // })
-
-
-      // Jsonp跨域
-      // let url = 'https://www.imooc.com/activity/servicetime';
-      // Jsonp(url,()=>{})
-
-      // 接口代理跨域
-      let url = '/activity/servicetime';
-      Jsonp(url,()=>{})
-
-    }
-
   }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/pages/order.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav-header></nav-header>
+    <router-view></router-view>
+    <nav-footer></nav-footer>
+</template>
+
+<script>
+    import NavHeader from "../components/NavHeader";
+    import NavFooter from "../components/NavFooter";
+    export default {
+        name: "order",
+        components: {
+            NavFooter,
+            NavHeader
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
