5a05145871cad50453c0c26041e17d8b6ac77186
ZAP <zhangaiping11011@163.com>
2020-04-23 23:12:11 +0800

    接口环境设置

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,16 @@
 </template>
 
 <script>
+  import storage from './storage'
   export default {
     name: "App",
+    mounted() {
+      // storage.setItem('a','1')                                      //添加一个属性值a:1
+      // storage.setItem('user',{a:1})
+      // storage.setItem('abc',{a:1},'user')      //向user里添加模块abc,值是a:1
+      storage.clear('a','user')      //把user里的a属性删除掉。
+
+    }
   }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from "vue-axios";
+import env from './env'
 
+// 接口错误拦截   前两行默认参数根据跨域方式做调整。
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000;
+//根据不同的环境请求不同的值。
+// axios.defaults.baseURL = env.baseURL;
+axios.interceptors.response.use((response)=>{
+  let res = response.data;
+  if (res.status == 0) {
+    return res.data;
+  }else if (res.status == 10) {
+    window.location.href = '/#/login';
+  }else {
+    alert(res.msg);
+  }
+})
+Vue.use(VueAxios,axios)
 Vue.config.productionTip = false
 
 new Vue({
